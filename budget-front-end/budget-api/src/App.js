a7fe790029cef8731c99f8b3effaf17b80fe438c
Raymond Udeogu <rfizzle25@gmail.com>
2021-06-28 14:48:00 -0400

    working on transaction pg

--- budget-front-end/budget-api/src/App.js
+++ budget-front-end/budget-api/src/App.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { apiURL } from "./util/apiURL";
 import NavBar from "./Components/NavBar"
+import Home from "./Pages/Home"
+import Index from "./Pages/Index"
+import New from "./Pages/New"
 import axios from "axios"
 
 const API_BASE = apiURL();
@@ -16,24 +19,6 @@ function App() {
       console.log(error)
     })
   }
-
-const deleteTransaction = (index) => {
-  axios.delete(`${API_BASE}/transactions/${index}`).then((res) => {
-    const updateArray = [...transactions]
-    updateArray.splice(index,1)
-    setTransactions(updateArray)
-  }).catch((error) => {
-    console.log(error)
-  })
-}
-
-const updateTransaction = (updatedTransaction, index) => {
-  axios.put(`${API_BASE}/transactions/:${index}`, updatedTransaction).then((res) =>{
-    const updateArray = [...transactions]
-    updateArray[index] = updatedTransaction
-    setTransactions(updateArray)
-  })
-}
 
 useEffect(() => {
   axios.get(`${API_BASE}/transactions`)
---
+++ budget-front-end/budget-api/src/Components/Transaction.js
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom"
+
+function Transaction({ transaction }) {
+    return (
+        
+    )
+}
